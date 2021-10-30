9ad0249e69b8b02b0b340d7532bf1924291f16b5
krisszzzz <89372621+krisszzzz@users.noreply.github.com>
2021-10-30 15:28:25 +0300

    Add files via upload

--- Compilator/main.cpp
+++ Compilator/main.cpp
@@ -1,19 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-#include "Compilator.h"
-//#include "TextLib.cpp" /// REMOVE!!!
-#include <sys/stat.h>
-#include "D:\ded_projects\Proccessor\log.h"
-//#include "D:\ded_projects\Proccessor\log.cpp" //REMOVE!!!
+#include "compilator.h"
+//#include "compilator.cpp"               //REMOVE
+//#include "cmptextlib.cpp"               //
+//#include "textlib.cpp"                  //
+//#include "..\proccessor_lib\log.cpp"    // Check collision of symbol with cmd code
 
-
-int main()
+int main(int argc, char* argv[])
 {
-    FILE* log                = fopen("log.txt", "w");
-    RememberLogFile(log);
-    
+    if(argc == 1) {
+        __PRINT_ALL_INFO__("Inccorect input -- please input in cmd the file path and name\n");
+        return -1;
+    }
+    //FILE* log                = fopen("log.txt", "w");
+    //RememberLogFile(log);
     compilation_file source  = {0};
-    CompilationFileCtor(&source, "source.txt");
+
+    if(CompilationFileCtor(&source, argv[1]) == ALREADY_COMPILATED)
+        return 0;
+        
     MakeStringsFromCompilationFile(&source);
     CompileCmd(&source);
     CompilationFileDtor(&source);
